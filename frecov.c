60310f8cd9f481707fb548980194de342f215cb9
tracer-nju <tracer@nju.edu.cn>
2019-05-27 10:52:52 +0800

    Static hostname: HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 69d42b12fc1d495180719c997faa8a77            Boot ID: c577881c415e41d6a7481496782e0426   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  10:52:52 up  1:00,  1 user,  load average: 0.30, 0.33, 0.38

--- frecov.c
+++ frecov.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
 			  if(cof == 0) continue;
 			  cof-=2;
 			  int picture = offset + pace*cof;
-			  printf("picture = %x %x %x\n",picture);
+			  printf("picture = %x\n",picture);
 			  if(buf[picture]!='B' || buf[picture+1]!='M')
 				  continue;
 			  int psize = *(int *)&buf[picture+2];
